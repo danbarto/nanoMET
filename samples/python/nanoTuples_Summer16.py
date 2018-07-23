6666024d3620726510db2134b759b0c9e7ffb647
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-07-23 17:06:01 +0200

    first slimmed down version of postprocessing and sample handling

--- samples/python/nanoTuples_Summer16.py
+++ samples/python/nanoTuples_Summer16.py
@@ -5,7 +5,19 @@ import ROOT
 dbFile = '/afs/hephy.at/data/dspitzbart01/nanoAOD/DB_Summer16.sql'
 dataDirectory = '/afs/hephy.at/data/dspitzbart01/nanoAOD/Fall17/'
 
+# specify a local directory if you want to create (and afterwards automatically use) a local copy of the sample, otherwise use the grid.
+DYJetsToLL_M50_LO = Sample.nanoAODfromDAS('DYJetsToLL_M50_LO','/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAOD-PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2_ext1-v1/NANOAODSIM', dbFile=dbFile, xSection=2008.*3)
 
-DYJetsToLL_M50_LO = Sample.nanoAODfromDAS('DYJetsToLL_M50_LO','/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAOD-PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2_ext1-v1/NANOAODSIM', localDir=dataDirectory, dbFile=dbFile)
 
+DY = [
+    DYJetsToLL_M50_LO,
+    ]
+
+dummy = [
+    ]
+
+allSamples = DY + dummy
+
+for s in allSamples:
+    s.isData = False
 
---
+++ tools/python/user.py
@@ -0,0 +1,10 @@
+import os
+
+if os.environ['USER'] in ['dspitzbart', 'dspitzba']:
+    results_directory       = "/afs/hephy.at/data/dspitzbart01/nanoMET/results/"
+    plot_directory          = "/afs/hephy.at/user/d/dspitzbart/www/nanoMET/"
+    dpm_directory           = '/dpm/oeaw.ac.at/home/cms/store/user/dspitzba/'
+    data_directory          = "/afs/hephy.at/data/dspitzbart02/metTuples/"
+    postprocessing_output_directory = "/afs/hephy.at/data/dspitzbart02/metTuples/"
+    analysis_results        = results_directory
+    
