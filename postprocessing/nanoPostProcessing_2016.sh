93bd8e8dcbc45953675bb6d5af517718024a4ca9
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-01-15 17:28:08 +0100

    clean-up

--- postprocessing/nanoPostProcessing_2016.sh
+++
@@ -1,8 +0,0 @@
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample DYJetsToLL_M50_LO 
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample DYJetsToLL_M50
-
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample TTLep_pow
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample WW WW_ext
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample WZ WZ_ext
-python nanoPostProcessing.py --year 2016 --skim dilep --fileBasedSplitting --processingEra 2016_v1  --sample ZZ ZZ_ext
-
