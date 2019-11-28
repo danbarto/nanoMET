e15d0e89400f4c21ee5dea80d2f464970c0f2d6c
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-11-28 14:54:03 +0100

    cleanup submission scripts

--- plots/analysisPlots.sh
+++ plots/analysisPlots.sh
@@ -1,75 +1,11 @@
-# pt 15
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016"
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016"
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016"
+python analysisPlots.py --calcMETSig --tuneEra 2016 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2016 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2016 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv22_v1
 
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016"
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016"
-runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016"
+python analysisPlots.py --calcMETSig --tuneEra 2017 --selection diMuon-looseLeptonVeto-onZ-BadEEJetVeto             --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2017 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0-BadEEJetVeto  --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2017 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p-BadEEJetVeto --plot_directory ppv22_v1
 
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169211525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016"
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016"
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20169211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016"
-
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016"
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016"
-#runNotifier.sh "python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016"
-
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ-BadEEJetVeto             --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p-BadEEJetVeto --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179214159 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0-BadEEJetVeto  --plot_directory ppv21_v6 --year 2017
-
-#python analysisPlots.py --calcMETSig --tuneEra 20189211525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20189211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20189211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2018
-
-# pt 25
-#python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20169212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016
-
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ-BadEEJetVeto             --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p-BadEEJetVeto --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20179212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0-BadEEJetVeto  --plot_directory ppv21_v6 --year 2017
-
-#python analysisPlots.py --calcMETSig --tuneEra 20189212525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20189212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20189212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2018
-
-# pt 15 pTdependent
-#python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20168211525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016
-
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ-BadEEJetVeto             --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p-BadEEJetVeto --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0-BadEEJetVeto  --plot_directory ppv21_v6 --year 2017
-
-#python analysisPlots.py --calcMETSig --tuneEra 20188211525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20188211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20188211525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2018
-
-# pt 25 pTdependent
-#python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ                          --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2016
-#python analysisPlots.py --calcMETSig --tuneEra 20168212525 --selection diMuonTune-tuneElectronVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2016
-
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ-BadEEJetVeto             --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p-BadEEJetVeto --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2017
-#python analysisPlots.py --calcMETSig --tuneEra 20178212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0-BadEEJetVeto  --plot_directory ppv21_v6 --year 2017
-
-#python analysisPlots.py --calcMETSig --tuneEra 20188212525 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20188212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv21_v6 --year 2018
-#python analysisPlots.py --calcMETSig --tuneEra 20188212525 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv21_v6 --year 2018
+python analysisPlots.py --calcMETSig --tuneEra 2018 --selection diMuon-looseLeptonVeto-onZ                          --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2018 --selection diMuon-looseLeptonVeto-onZ-nCleanJet0               --plot_directory ppv22_v1
+python analysisPlots.py --calcMETSig --tuneEra 2018 --selection diMuon-looseLeptonVeto-onZ-nCleanJet1p              --plot_directory ppv22_v1
